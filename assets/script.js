8d62b2dee9a652336a09011f0a55c66761148140
scaratwork <104857468+scaratwork@users.noreply.github.com>
2022-05-11 16:56:33 -0300

    Add files via upload
    new functions, lockboard controller

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
 };
 
-cards.forEach((card)=> 
-card.addEventListener('click', flipCard)
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCard();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCard() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500); 
+}
+
+cards.forEach(card => 
+    card.addEventListener('click', flipCard)
 );
+
